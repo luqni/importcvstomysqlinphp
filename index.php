cdfcf46b74c46ce138c407a0dc43c82bdc701eab
Muhammad Luqni Baehaqi <mluqnibaehaqi@gmail.com>
2018-03-07 19:25:08 +0700

    second commit

--- index.php
+++ index.php
@@ -7,6 +7,40 @@
 	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
+<?php
+	function get_all_records(){
+	    $con = getdb();
+	    $Sql = "SELECT * FROM employeeinfo";
+	    $result = mysqli_query($con, $Sql);  
+
+
+	    if (mysqli_num_rows($result) > 0) {
+	     echo "<div class='table-responsive'><table id='myTable' class='table table-striped table-bordered'>
+	             <thead><tr><th>EMP ID</th>
+	                          <th>First Name</th>
+	                          <th>Last Name</th>
+	                          <th>Email</th>
+	                          <th>Registration Date</th>
+	                        </tr></thead><tbody>";
+
+
+		    while($row = mysqli_fetch_assoc($result))
+			    {
+
+			         echo "<tr><td>" . $row['emp_id']."</td>
+			                   <td>" . $row['firstname']."</td>
+			                   <td>" . $row['lastname']."</td>
+			                   <td>" . $row['email']."</td>
+			                   <td>" . $row['reg_date']."</td></tr>";        
+			    }
+    
+    	echo "</tbody></table></div>";
+     
+		} else {
+     	echo "you have no records";
+		}
+	}
+?>	 
 	<div id="wrap">
         <div class="container">
             <div class="row">
@@ -42,5 +76,15 @@
             ?>
         </div>
     </div>
+     <div>
+            <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"   
+                      enctype="multipart/form-data">
+                  <div class="form-group">
+                            <div class="col-md-4 col-md-offset-4">
+                                <input type="submit" name="Export" class="btn btn-success" value="export to excel"/>
+                            </div>
+                   </div>                    
+            </form>           
+ </div>
 </body>
 </html>
